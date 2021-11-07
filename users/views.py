b08cdbe783db8f430843d193cd55ad45418cc25a
domo <rtdokmo@gmail.com>
2021-11-07 21:31:55 +0300

    07/11/21
    add register forms and user model

---
+++ users/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+from users.forms import UserLoginForm
+
+
+def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        print('check')
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                print('all good')
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+        print('so bad')
+    context = {
+        'form': form
+    }
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    return render(request, 'users/register.html')
+
+# Create your views here.
